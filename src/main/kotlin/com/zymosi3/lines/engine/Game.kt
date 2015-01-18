48df4948f9409bfa94c65e01fc0dbe8bea94032e
Anton Tarasenko <zymosi3@gmail.com>
2015-01-18 22:49:06 +0700

    engine works + tests

--- src/main/kotlin/com/zymosi3/lines/engine/Game.kt
+++ src/main/kotlin/com/zymosi3/lines/engine/Game.kt
@@ -3,6 +3,8 @@ package com.zymosi3.lines.engine
 import java.util.ArrayList
 import java.util.Random
 import java.util.LinkedList
+import java.util.HashSet
+import kotlin.browser.document
 
 public enum class Color {
     red
@@ -14,30 +16,58 @@ public enum class Color {
     burgundy
 }
 
-public class Ball(color: Color) {
-    public val color: Color = color
-}
+public class Ball(val color: Color)
 
-public class Cell(x: Int, y: Int) {
+public class Cell(val x: Int, val y: Int) {
 
-    public val x: Int = x
-    public val y: Int = y
     public var ball: Ball? = null
         internal set
 }
 
-public class Field(size: Int) :  Iterable<Cell> {
+public enum class Direction {
+    left
+    right
+    top
+    bot
+}
 
-    public val size: Int = size
+public class Field(val size: Int) :  Iterable<Cell> {
+
     internal val cells: List<Cell> = Array(size * size, {i -> Cell(i / size, i % size)}).toList()
     private val cellsMap: Map<Pair<Int, Int>, Cell> = cells.toMap({cell -> Pair(cell.x, cell.y)})
 
-    public fun cell(x: Int, y: Int): Cell = cellsMap.get(Pair(x, y)) ?: throw IllegalArgumentException("Can't find cell {$x, $y}")
+    public fun cell(x: Int, y: Int): Cell? = cellsMap.get(Pair(x, y))
+
+    public fun left(cell: Cell): Cell? = cell(cell.x - 1, cell.y)
+
+    public fun right(cell: Cell): Cell? = cell(cell.x + 1, cell.y)
+
+    public fun top(cell: Cell): Cell? = cell(cell.x, cell.y - 1)
+
+    public fun bot(cell: Cell): Cell? = cell(cell.x, cell.y + 1)
+
+    public fun cell(cell: Cell, dir: Direction): Cell? =
+        when (dir) {
+            Direction.left -> left(cell)
+            Direction.right -> right(cell)
+            Direction.top -> top(cell)
+            Direction.bot -> bot(cell)
+        }
 
     override fun iterator(): Iterator<Cell> = cells.iterator()
 }
 
-public class Game(size: Int, startBalls: Int) {
+public data class MoveResult(
+        val success: Boolean,
+        val purged: Set<Cell>,
+        val gameFinished: Boolean
+)
+
+public class Game(size: Int, startBalls: Int, randomSeed: Long? = null) {
+
+    // constants
+    private val nextBallsCount = 3
+    private val minLineSize = 5
 
     private val startBalls: Int = startBalls
     public val field: Field = Field(size)
@@ -45,22 +75,182 @@ public class Game(size: Int, startBalls: Int) {
         private set
     public var currentScore: Int = 0
         private set
+    public var movesNum:Int = 0
+        private set
 
-    private val occupied: MutableList<Cell> = LinkedList()
-    private val free: MutableList<Cell> = LinkedList(field.cells)
+    internal val occupied: MutableList<Cell> = LinkedList()
+    internal val free: MutableList<Cell> = LinkedList(field.cells)
 
-    private val rand: Random = Random()
+    private val rand: Random = if (randomSeed != null) Random(randomSeed) else Random()
 
-    public fun start() {
+    public fun start(): MoveResult {
         for (i in 1..startBalls) {
-            val index = rand.nextInt(free.size())
-            val cell = free.get(index)
-            cell.ball = Ball(randColor())
-            free.remove(index)
-            occupied.add(cell)
+            putRandom(Ball(randColor()))
         }
-        // TODO what if we have 5 in row now?
+        nextBalls = defineNextBalls()
+        return MoveResult(true, purge(), free.size() <= nextBalls.size())
     }
+
+    public fun move(from: Cell, to: Cell): MoveResult {
+        if (isLegal(from, to)) {
+            movesNum++
+            to.ball = from.ball
+            from.ball = null
+            occupied.remove(from)
+            occupied.add(to)
+            free.add(from)
+            free.remove(to)
+            val purged = purge()
+            if (purged.isNotEmpty()) {
+                // update score
+                currentScore += minLineSize
+                if (purged.size() > minLineSize) {
+                    for (i in minLineSize + 1..purged.size()) {
+                        currentScore += (i - minLineSize) * (i - minLineSize + 1)
+                    }
+                }
+            }
+            if (free.size() > nextBalls.size()) {
+                if (purged.isEmpty()) {
+                    // put new balls
+                    for (ball in nextBalls) {
+                        putRandom(ball)
+                    }
+                    val purgedAgain = purge()
+                    nextBalls = defineNextBalls()
+                    return MoveResult(true, purgedAgain, false)
+                } else {
+                    return MoveResult(true, purged, false)
+                }
+            } else {
+                // game over
+                return MoveResult(true, purged, true)
+            }
+        } else {
+            // illegal move
+            return MoveResult(false, emptySet(), free.size() <= nextBalls.size())
+        }
+    }
+
+    private fun isLegal(from: Cell, to: Cell): Boolean {
+        // using something like interpretation of Dijkstra algorithm
+        fun explore(cur: Cell, explored: Collection<Cell>, toExplore: MutableList<Pair<Cell, Direction>>) {
+            for (dir in Direction.values()) {
+                val cell = field.cell(cur, dir)
+                if (cell != null && !explored.contains(cell))
+                    toExplore.add(Pair(cur, dir))
+            }
+        }
+
+        fun isLegal(from: Cell, to: Cell, explored: MutableCollection<Cell>, toExplore: MutableList<Pair<Cell, Direction>>): Boolean {
+            if (from == to)
+                return true
+            explored.add(from)
+            explore(from, explored, toExplore)
+            if (toExplore.isEmpty())
+                return false
+            val next = toExplore.get(0)
+            toExplore.remove(0)
+            return isLegal(field.cell(next.first, next.second) as Cell, to, explored, toExplore)
+        }
+        return isLegal(from, to, HashSet(occupied), LinkedList())
+    }
+
+    private fun purge(): Set<Cell> {
+        val purged: MutableSet<Cell> = HashSet()
+
+        fun purgeLine(line: List<Cell>) {
+            if (line.size() >= minLineSize)
+                purged.addAll(line)
+        }
+
+        fun addToLine(cell: Cell, line: MutableList<Cell>) {
+            if (cell.ball != null)
+                if (line.isEmpty())
+                    // no matter what color, line is empty
+                    line.add(0, cell)
+                else if (line.get(0).ball?.color == cell.ball?.color)
+                    // line is not empty we need the same color
+                    line.add(0, cell)
+                else {
+                    purgeLine(line)
+                    // we have another color so clear current line and start new
+                    line.clear()
+                    line.add(0, cell)
+                }
+            else
+                purgeLine(line)
+        }
+        // going row by row
+        for (i in 0..field.size - 1) {
+            val line: MutableList<Cell> = LinkedList()
+            for (j in 0..field.size - 1)
+                addToLine(field.cell(j, i) as Cell, line)
+            purgeLine(line)
+        }
+        // going column by column
+        for (i in 0..field.size - 1) {
+            val line: MutableList<Cell> = LinkedList()
+            for (j in 0..field.size - 1)
+                addToLine(field.cell(i, j) as Cell, line)
+            purgeLine(line)
+        }
+        // going diagonals from right to left
+        for (k in 0..field.size - 1) {
+            val line: MutableList<Cell> = LinkedList()
+            for (l in 0..field.size - 1 - k) {
+                val i = l
+                val j = k + l
+                addToLine(field.cell(i, j) as Cell, line)
+            }
+            purgeLine(line)
+        }
+        for (k in 1..field.size - 1) {
+            val line: MutableList<Cell> = LinkedList()
+            for (l in k..field.size - 1) {
+                val i = l
+                val j = l - k
+                addToLine(field.cell(i, j) as Cell, line)
+            }
+            purgeLine(line)
+        }
+        // going diagonals from left to right
+        for (k in 0..field.size - 1) {
+            val line: MutableList<Cell> = LinkedList()
+            for (l in k downTo 0) {
+                val i = l
+                val j = k - l
+                addToLine(field.cell(i, j) as Cell, line)
+            }
+            purgeLine(line)
+        }
+        for (k in 1..field.size - 1) {
+            val line: MutableList<Cell> = LinkedList()
+            for (l in field.size - 1 downTo k) {
+                val i = l
+                val j = field.size - 1 - l + k
+                addToLine(field.cell(i, j) as Cell, line)
+            }
+            purgeLine(line)
+        }
+        // clear purged cells
+        for (cell in purged) {
+            cell.ball = null
+            free.add(cell)
+            occupied.remove(cell)
+        }
+        return purged
+    }
+
+    private fun putRandom(ball: Ball) {
+        val index = rand.nextInt(free.size())
+        val cell = free.get(index)
+        cell.ball = ball
+        free.remove(index)
+        occupied.add(cell)
+    }
+
+    private fun defineNextBalls(): List<Ball> = Array(nextBallsCount, {i -> Ball(randColor())}).toList()
 
     private fun randColor(): Color = Color.values().get(rand.nextInt(Color.values().size()))
 }
---
+++ src/test/kotlin/com/zymosi3/lines/engine/GameTest.kt
@@ -0,0 +1,231 @@
+package com.zymosi3.lines.engine
+
+import org.junit.Test as test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+import kotlin.test.assertFalse
+import java.util.Random
+import kotlin.test.assertNull
+import kotlin.test.assertNotNull
+
+public class GameTest {
+
+    val size = 9
+    val startBalls = 6
+    val seed = 42L
+
+    test fun createGameTest() {
+        val game = Game(size, startBalls, seed)
+        assertEquals(size, game.field.size)
+        assertTrue(game.nextBalls.isEmpty())
+        assertEquals(0, game.currentScore)
+        assertTrue(game.occupied.isEmpty())
+        assertEquals(size * size, game.free.size())
+    }
+
+    test fun startGameTest() {
+        val game = Game(size, startBalls, seed)
+        val res = game.start()
+        assertTrue(res.success)
+        assertTrue(res.purged.isEmpty())
+        assertFalse(res.gameFinished)
+        assertEquals(3, game.nextBalls.size())
+        assertEquals(0, game.currentScore)
+        assertEquals(startBalls, game.occupied.size())
+        assertEquals(size * size - startBalls, game.free.size())
+    }
+
+    test fun finishGameTest() {
+        val game = Game(size, startBalls, seed)
+        game.start()
+        val rand = Random(seed)
+        fun randomMove(): MoveResult = game.move(
+                game.occupied.get(rand.nextInt(game.occupied.size())),
+                game.free.get(rand.nextInt(game.free.size()))
+        )
+        var res: MoveResult
+        while (true) {
+            res = randomMove()
+            while (! res.success) {
+                res = randomMove()
+            }
+            if (res.gameFinished) {
+                break
+            }
+        }
+        assertTrue(res.gameFinished)
+        assertEquals(3, game.free.size())
+        assertEquals(78, game.occupied.size())
+        assertEquals(0, game.currentScore)
+        assertEquals(25, game.movesNum)
+    }
+
+    test fun moveTest() {
+        val game = Game(size, startBalls, seed)
+        game.start()
+        val field = game.field
+        val from = field.cell(3, 3) as Cell
+        val to = field.cell(5, 2) as Cell
+
+        assertTrue(game.occupied.contains(from))
+        assertFalse(game.occupied.contains(to))
+        assertFalse(game.free.contains(from))
+        assertTrue(game.free.contains(to))
+        assertNotNull(from.ball)
+        assertNull(to.ball)
+
+        val res = game.move(from, to)
+
+        assertTrue(res.success)
+        assertTrue(res.purged.isEmpty())
+        assertFalse(res.gameFinished)
+        assertFalse(game.occupied.contains(from))
+        assertTrue(game.occupied.contains(to))
+        assertTrue(game.free.contains(from))
+        assertFalse(game.free.contains(to))
+        assertNull(from.ball)
+        assertNotNull(to.ball)
+    }
+
+    test fun purgeRowTest() {
+        val game = Game(size, startBalls, seed)
+        game.start()
+        val field = game.field
+        val line = listOf(field.cell(1, 2), field.cell(2, 2), field.cell(3, 2), field.cell(4, 2))
+        for (cell in line) {
+            cell?.ball = Ball(Color.blue)
+            game.occupied.add(cell as Cell)
+            game.free.remove(cell)
+        }
+        val from = field.cell(3, 3) as Cell
+        val to = field.cell(5, 2) as Cell
+        val res = game.move(from, to)
+
+        assertTrue(res.success)
+        assertEquals(5, res.purged.size())
+        assertFalse(res.gameFinished)
+        assertEquals(5, game.currentScore)
+    }
+
+    test fun scoreTest() {
+        val game = Game(size, startBalls, seed)
+        game.start()
+        val field = game.field
+        val line = listOf(field.cell(1, 2), field.cell(2, 2), field.cell(3, 2), field.cell(4, 2), field.cell(6, 2), field.cell(7, 2), field.cell(8, 2))
+        for (cell in line) {
+            cell?.ball = Ball(Color.blue)
+            game.occupied.add(cell as Cell)
+            game.free.remove(cell)
+        }
+        val from = field.cell(3, 3) as Cell
+        val to = field.cell(5, 2) as Cell
+        val res = game.move(from, to)
+
+        assertTrue(res.success)
+        assertEquals(8, res.purged.size())
+        assertFalse(res.gameFinished)
+        assertEquals(25, game.currentScore)
+    }
+
+    test fun purgeColumnTest() {
+        val game = Game(size, startBalls, seed)
+        game.start()
+        val field = game.field
+        val line = listOf(field.cell(4, 0), field.cell(4, 1), field.cell(4, 2), field.cell(4, 5))
+        for (cell in line) {
+            cell?.ball = Ball(Color.blue)
+            game.occupied.add(cell as Cell)
+            game.free.remove(cell)
+        }
+        val from = field.cell(3, 3) as Cell
+        val to = field.cell(4, 3) as Cell
+        val res = game.move(from, to)
+
+        assertTrue(res.success)
+        assertEquals(5, res.purged.size())
+        assertFalse(res.gameFinished)
+        assertEquals(5, game.currentScore)
+    }
+
+    test fun purgeDiagonalTest1() {
+        val game = Game(size, startBalls, seed)
+        game.start()
+        val field = game.field
+        val line = listOf(field.cell(0, 2), field.cell(1, 3), field.cell(2, 4), field.cell(4, 6))
+        for (cell in line) {
+            cell?.ball = Ball(Color.blue)
+            game.occupied.add(cell as Cell)
+            game.free.remove(cell)
+        }
+        val from = field.cell(3, 3) as Cell
+        val to = field.cell(3, 5) as Cell
+        val res = game.move(from, to)
+
+        assertTrue(res.success)
+        assertEquals(5, res.purged.size())
+        assertFalse(res.gameFinished)
+        assertEquals(5, game.currentScore)
+    }
+
+    test fun purgeDiagonalTest2() {
+        val game = Game(size, startBalls, seed)
+        game.start()
+        val field = game.field
+        val line = listOf(field.cell(4, 1), field.cell(5, 2), field.cell(6, 3), field.cell(8, 5))
+        for (cell in line) {
+            cell?.ball = Ball(Color.blue)
+            game.occupied.add(cell as Cell)
+            game.free.remove(cell)
+        }
+        val from = field.cell(3, 3) as Cell
+        val to = field.cell(7, 4) as Cell
+        val res = game.move(from, to)
+
+        assertTrue(res.success)
+        assertEquals(5, res.purged.size())
+        assertFalse(res.gameFinished)
+        assertEquals(5, game.currentScore)
+    }
+
+    test fun purgeDiagonalTest3() {
+        val game = Game(size, startBalls, seed)
+        game.start()
+        val field = game.field
+        val line = listOf(field.cell(6, 2), field.cell(5, 3), field.cell(4, 4), field.cell(2, 6), field.cell(1, 7))
+        for (cell in line) {
+            cell?.ball = Ball(Color.blue)
+            game.occupied.add(cell as Cell)
+            game.free.remove(cell)
+        }
+        val from = field.cell(3, 3) as Cell
+        val to = field.cell(3, 5) as Cell
+        val res = game.move(from, to)
+
+        assertTrue(res.success)
+        assertEquals(6, res.purged.size())
+        assertFalse(res.gameFinished)
+        assertEquals(7, game.currentScore)
+    }
+
+    test fun purgeDiagonalTest4() {
+        val game = Game(size, startBalls, seed)
+        game.start()
+        val field = game.field
+        val line = listOf(field.cell(8, 4), field.cell(7, 5), field.cell(5, 7), field.cell(4, 8))
+        for (cell in line) {
+            cell?.ball = Ball(Color.blue)
+            game.occupied.add(cell as Cell)
+            game.free.remove(cell)
+        }
+        val from = field.cell(3, 3) as Cell
+        val to = field.cell(6, 6) as Cell
+        val res = game.move(from, to)
+
+        assertTrue(res.success)
+        assertEquals(5, res.purged.size())
+        assertFalse(res.gameFinished)
+        assertEquals(5, game.currentScore)
+    }
+}
+
+
